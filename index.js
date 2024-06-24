5d839a30743f5867dc3bbf6b376beac91e0ae4c5
tehreem131 <tehreemkhanzada23@gmail.com>
2024-06-24 15:16:10 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//printing a wellcome massage
+console.log("\n\tWellcome To \`tehreemusama\` - CLI Simple Calculator\n");
+let answer = await inquirer.prompt([
+    { massage: "Enter first number", type: "number", name: "firstNumber" },
+    { massage: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        massage: "Select one of tha operators to perform operators",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(answer);
+//conditional statment
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid operator");
+}
